8217de0fb83dbef0e6338cdd71bd40c742efe837
Mostafa Zakri <mostafazakri3@gmail.com>
2024-09-12 12:03:37 +0100

    Ascii Art Version 3

--- artistTools/printLineAsAscii.go
+++ artistTools/printLineAsAscii.go
@@ -3,11 +3,11 @@ package ascii
 import "fmt"
 
 // PrintLineAsAscii prints a given line of text as ASCII art using a provided ASCII graph.
-// The line parameter represents the text to be printed as ASCII art.
-// The asciiGraph parameter is a slice of strings that represents the ASCII graph used for printing.
-// Each character in the line is converted to ASCII art by looping through it to the corresponding lines in the asciiGraph.
-// The ASCII art is printed line by line, with each line of the ASCII art represented by a string in the asciiGraph.
-// If the line parameter is empty, a new line is printed.
+// the line parameter represents the text to be printed as ASCII art.
+// the asciiGraph parameter is a slice of strings that represents the ASCII graph used for printing.
+// each character in the line is converted to ASCII art by looping through it to the corresponding lines in the asciiGraph and storing it in asciiChars.
+// the ASCII art is printed line by line, with each line of the ASCII art represented by a string in the asciiChars.
+// if the line parameter is empty, a new line is printed.
 func PrintLineAsAscii(line string, asciiGraph []string) {
 	var asciiChars []string
 	if line != "" {
