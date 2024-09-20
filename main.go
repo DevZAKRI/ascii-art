dd2be947fea66711933e94a05c335fa4fa1d1d45
mZAKRI <mostafazakri3@gmail.com>
2024-09-20 16:00:18 +0100

    test

--- main.go
+++ main.go
@@ -7,18 +7,6 @@ import (
 	ascii "ascii/artistTools"
 )
 
-var ColorMap = map[string]string{
-	"black":     "\033[30m",
-	"red":       "\033[31m",
-	"green":     "\033[32m",
-	"yellow":    "\033[33m",
-	"blue":      "\033[34m",
-	"magenta":   "\033[35m",
-	"cyan":      "\033[36m",
-	"white":     "\033[37m",
-	"ColorSTOP": "\033[0m",
-}
-
 // main is the entry point of the program.
 // it validates the input argument, prints any validation errors,
 // and then calls the Artist function to process the input.
@@ -28,7 +16,6 @@ func main() {
 		fmt.Println(ascii.ValidateArgument(os.Args))
 		return
 	}
-
 	input := os.Args[1]
 	// take the input and and Process it
 	ascii.Artist(input)
