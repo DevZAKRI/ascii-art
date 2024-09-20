918c1e45acc74d456c40bb6ab08c5ca441f97826
Mostafa ZAKRI <32000899+DevZAKRI@users.noreply.github.com>
2024-09-20 15:37:19 +0100

    color semi done

--- artistTools/theartist.go
+++ artistTools/theartist.go
@@ -2,6 +2,7 @@ package ascii
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -9,21 +10,53 @@ import (
 // It reads the file containing the ASCII graph representation and puts the content in asciiGraph.
 // If there is an error reading the file, it prints the error message and returns.
 // It splits the input string into lines using the "\\n" delimiter.
-func Artist(input string) {
+func Artist(input string, template string) string {
 	// check for errors too in case file no longer exist
-	asciiGraph, err := ReadFile("banners/standard.txt")
+	asciiGraph, err := ReadFile("banners/" + template + ".txt")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
-		return
+		os.Exit(1)
 	}
-
+	for idx, LINE := range asciiGraph {
+		asciiGraph[idx] = strings.ReplaceAll(LINE, "\r", "")
+	}
 	lines := strings.Split(input, "\\n")
 	// If the input string is empty after removing "\\n", it removes the first line.
 	if strings.ReplaceAll(input, "\\n", "") == "" {
 		lines = lines[1:]
 	}
+	finalAsciiArt := ""
 	for _, line := range lines {
-		// prints each line as ASCII art using the PrintLineAsAscii function.
-		PrintLineAsAscii(line, asciiGraph)
+		// add each line to finalAsciiArt using the LineAsAscii function.
+		finalAsciiArt += LineAsAscii(line, asciiGraph)
 	}
+	return finalAsciiArt
+}
+
+func LineAsAscii(line string, asciiGraph []string) string {
+	var asciiChars []string
+	finalAsciiArt := ""
+	if line != "" {
+		for _, char := range line {
+			// add each chars to the asciiChars line by line
+			for i := 8; i > 0; i-- {
+				asciiChars = append(asciiChars, string(asciiGraph[findLastLine(char)-i]))
+			}
+		}
+
+		for i := 0; i < 8; i++ {
+			for j := 0; j < len(asciiChars); j += 8 {
+				finalAsciiArt += asciiChars[i+j]
+			}
+			finalAsciiArt += "\n"
+		}
+	} else {
+		finalAsciiArt += "\n"
+	}
+	return finalAsciiArt
+}
+
+// find the last line after the char
+func findLastLine(char rune) int {
+	return int((char - 31) * (9))
 }
